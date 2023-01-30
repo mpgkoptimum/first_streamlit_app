0bfb633bff2043a9c950e5f0a9795c01e68c27f1
mpgkoptimum <123889699+mpgkoptimum@users.noreply.github.com>
2023-01-30 08:00:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ streamlit.dataframe (fruits_to_show)
 
 # new section to display fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!")
+#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())

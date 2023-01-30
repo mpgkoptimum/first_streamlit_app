f289d342a93af20db90e28cc6d2d26494d11d74e
mpgkoptimum <123889699+mpgkoptimum@users.noreply.github.com>
2023-01-30 07:52:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected] #display
 streamlit.dataframe (fruits_to_show)
 
 # new section to display fruity vice api response
-
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 

68d54916f106575fda3121732bf83ac70c696830
mpgkoptimum <123889699+mpgkoptimum@users.noreply.github.com>
2023-01-31 07:31:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 
